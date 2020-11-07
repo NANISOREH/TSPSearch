062f4008fbaa0b2e34373c540e2376f8aa947023
NANISOREH <>
2020-11-07 18:40:10 +0100

    TspSolver refactoring

--- src/tsp/Main.java
+++ src/tsp/Main.java
@@ -16,17 +16,10 @@ public class Main {
         Instant start;
         Instant finish;
         Tour solution;
+        TspSolver solver = new TspSolver();
 
         start = Instant.now();
-        solution = TspSolver.solve(cities, Algorithm.GENETIC_SEARCH);
-        finish = Instant.now();
-        System.out.println("Genetic Search Approach:");
-        System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + " milliseconds");
-        System.out.println("Final tour length: " + solution.getDistance());
-        System.out.println("Tour: " + solution + "\n\n");
-
-        start = Instant.now();
-        solution = TspSolver.solve(cities, Algorithm.HILL_CLIMBING);
+        solution = solver.solve(cities, Algorithm.HILL_CLIMBING);
         finish = Instant.now();
         System.out.println("Hill Climbing:");
         System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + " milliseconds");
@@ -34,7 +27,8 @@ public class Main {
         System.out.println("Tour: " + solution + "\n\n");
 
         start = Instant.now();
-        solution = TspSolver.solve(cities, Algorithm.HILL_CLIMBING_RANDOM_RESTARTS);
+        solver.configureHCRR(10);
+        solution = solver.solve(cities, Algorithm.HILL_CLIMBING_RANDOM_RESTARTS);
         finish = Instant.now();
         System.out.println("Hill Climbing with 10 random restarts:");
         System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + " milliseconds");
@@ -42,15 +36,24 @@ public class Main {
         System.out.println("Tour: " + solution + "\n\n");
 
         start = Instant.now();
-        solution = TspSolver.solve(cities, Algorithm.SIMULATED_ANNEALING);
+        solution = solver.solve(cities, Algorithm.SIMULATED_ANNEALING);
         finish = Instant.now();
         System.out.println("Simulated Annealing:");
         System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + " milliseconds");
         System.out.println("Final tour length: " + solution.getDistance());
         System.out.println("Tour: " + solution + "\n\n");
 
+        start = Instant.now();
+        solver.configureGS(2000, 2, 0.01, 1.0, 600, 3600000L);
+        solution = solver.solve(cities, Algorithm.GENETIC_SEARCH);
+        finish = Instant.now();
+        System.out.println("Genetic Search Approach:");
+        System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + " milliseconds");
+        System.out.println("Final tour length: " + solution.getDistance());
+        System.out.println("Tour: " + solution + "\n\n");
+
 /*        start = Instant.now();
-        solution = TspSolver.solve(cities, Algorithm.BRUTE_FORCE);
+        solution = solver.solve(cities, Algorithm.BRUTE_FORCE);
         finish = Instant.now();
         System.out.println("Brute Force:");
         System.out.println("Solution obtained in " + Duration.between(start, finish).toMillis() + "milliseconds");
